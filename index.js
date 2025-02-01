2b21730df3e000224acc27649ed2ed55c29ad514
Alexandr Evdokimov <tohoku@yandex.ru>
2025-02-01 09:04:43 +0300

    x

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf(BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Привет'))
-bot.help((ctx) => ctx.reply('Я копирую любое отправленное сообщение'))
+bot.help((ctx) => ctx.reply('Я копирую любое ваше отправленное сообщение'))
 bot.on('message', (ctx) => {
     ctx.telegram.sendCopy(ctx.chat.id, ctx.message)
 })
